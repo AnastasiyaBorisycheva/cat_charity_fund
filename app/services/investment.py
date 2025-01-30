fae4247229e92259f23ed8b4a371ae0cae27b4a0
Anastasiya Borisycheva <borisycheva.a@yandex.ru>
2025-01-30 15:41:02 +0300

    PEP edited

--- app/services/investment.py
+++ app/services/investment.py
@@ -12,32 +12,40 @@ async def investing_magic(
     opened_projects = await session.execute(
         select(CharityProject).where(CharityProject.fully_invested == 0)
     )
-
     opened_project = opened_projects.scalars().first()
 
     opened_donations = await session.execute(
         select(Donation).where(Donation.fully_invested == 0)
     )
-
     opened_donation = opened_donations.scalars().first()
 
     while opened_project is not None and opened_donation is not None:
-        delta_project = opened_project.full_amount - opened_project.invested_amount
-        delta_donation = opened_donation.full_amount - opened_donation.invested_amount
+        delta_project = (
+            opened_project.full_amount - opened_project.invested_amount
+        )
+        delta_donation = (
+            opened_donation.full_amount - opened_donation.invested_amount
+        )
 
         if delta_project > delta_donation:
-            new_project_invested_amount = opened_project.invested_amount + delta_donation
-            setattr(opened_project, 'invested_amount', new_project_invested_amount)
+            new_project_amount = (
+                opened_project.invested_amount + delta_donation
+            )
+            setattr(opened_project, 'invested_amount', new_project_amount)
 
-            new_donation_invested_amount = opened_donation.invested_amount + delta_donation
-            setattr(opened_donation, 'invested_amount', new_donation_invested_amount)
+            new_donation_amount = (
+                opened_donation.invested_amount + delta_donation
+            )
+            setattr(opened_donation, 'invested_amount', new_donation_amount)
 
         elif delta_project <= delta_donation:
-            new_project_invested_amount = opened_project.invested_amount + delta_project
-            setattr(opened_project, 'invested_amount', new_project_invested_amount)
+            new_project_amount = opened_project.invested_amount + delta_project
+            setattr(opened_project, 'invested_amount', new_project_amount)
 
-            new_donation_invested_amount = opened_donation.invested_amount + delta_project
-            setattr(opened_donation, 'invested_amount', new_donation_invested_amount)
+            new_donation_amount = (
+                opened_donation.invested_amount + delta_project
+            )
+            setattr(opened_donation, 'invested_amount', new_donation_amount)
 
         if opened_project.invested_amount == opened_project.full_amount:
             setattr(opened_project, 'fully_invested', True)
